7a1a6c222a042417bd1e8a9818b34a8f9e8ae376
Nitanta Adhikari <nitanta.adh007@gmail.com>
2021-08-23 16:56:19 +0545

    SOLID, Creational

---
+++ Patterns/SOLID/ISP.swift
@@ -0,0 +1,109 @@
+//
+//  ISP.swift
+//  Design_patterns
+//
+//  Created by Nitanta Adhikari on 8/22/21.
+//
+
+import Foundation
+
+class Document
+{
+
+}
+
+
+protocol Machine
+{
+  func print(d: Document)
+  func scan(d: Document)
+  func fax(d: Document)
+}
+
+class MFP : Machine
+{
+    func scan(d: Document) {
+    }
+    
+    func fax(d: Document) {
+    }
+    
+    func print(d: Document) {}
+  //
+}
+
+enum NoRequiredFunctionality : Error
+{
+  case doesNotFax
+}
+
+class OldFashionedPrinter: Machine
+{
+    func scan(d: Document) {
+    }
+    
+  func print(d: Document)
+  {
+  }
+  func fax(d: Document)
+  {
+    throw NoRequiredFunctionality.doesNotFax
+  }
+}
+
+protocol Printer
+{
+  func print(d: Document)
+}
+
+protocol Scanner
+{
+  func scan(d: Document)
+}
+
+protocol Fax
+{
+  func fax(d: Document)
+}
+
+class OrdinaryPrinter : Printer
+{
+  func print(d: Document)
+  {
+    // ok
+  }
+}
+
+class Photocopier : Printer, Scanner
+{
+  func print(d: Document)
+  {
+    // ok
+  }
+
+  func scan(d: Document)
+  {
+
+  }
+}
+
+protocol MultiFunctionDevice : Printer, Scanner, Fax
+{
+}
+
+class MultiFunctionMachine : MultiFunctionDevice
+{
+  let printer: Printer
+  let scanner: Scanner
+
+  init(printer: Printer, scanner: Scanner)
+  {
+    self.printer = printer
+    self.scanner = scanner
+  }
+
+  func print(d: Document)
+  {
+    printer.print(d) // Decorator
+  }
+}
