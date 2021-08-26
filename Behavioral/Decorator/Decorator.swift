50cd1fa08215dd69f6f5f76331921cf4737ba82d
Nitanta Adhikari <nitanta.adh007@gmail.com>
2021-08-26 22:42:16 +0545

    Bahavioral design patterns

---
+++ Behavioral/Decorator/Decorator.swift
@@ -0,0 +1,8 @@
+//
+//  Decorator.swift
+//  Design patterns
+//
+//  Created by Nitanta Adhikari on 8/26/21.
+//
+
+import Foundation
---
+++ Behavioral/Proxy/Proxy.swift
@@ -0,0 +1,8 @@
+//
+//  Proxy.swift
+//  Design patterns
+//
+//  Created by Nitanta Adhikari on 8/26/21.
+//
+
+import Foundation
